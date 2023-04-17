227f0ddb1bcc8c7d812161981de6ed1837613df6
RudinP <gin04110@gmail.com>
2023-04-17 09:35:10 +0900

    230417: Roll a Ball 완료

--- UnityProjects/Roll a Ball/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
 
+    public Text countText;
+
+    public Text winText;
+
+    public Text scoreText;
+ 
     private Rigidbody rb;
+
+    private int count;
+
+    private int score;
     
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        SetCountText();
+        winText.text = "";
     }
     
     private void FixedUpdate()
@@ -19,6 +34,40 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+
+            switch(other.gameObject.name)
+            {
+                case "Pick Up": score += 10; break;
+                case "Pick Up 2": score += 20; break;
+                case "Pick Up 3": score += 30; break;
+                default: break;
+            }
+
+            SetScoreText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
     }
 
 }
