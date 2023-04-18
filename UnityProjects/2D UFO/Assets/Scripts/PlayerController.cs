d3e81802e8964e1368d2fe7f900e35baedf0b3f5
RudinP <gin04110@gmail.com>
2023-04-18 15:11:26 +0900

    230418: 2D UFO 완성

--- UnityProjects/2D UFO/Assets/Scripts/PlayerController.cs
+++ UnityProjects/2D UFO/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,10 +10,43 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb2d;
 
+    private int count;
+    private int score;
+
+    private bool isEnd;
+
+    public TMP_Text countText;
+    public TMP_Text winText;
+    public TMP_Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        
+        count = 0;
+        score = 0;
+        
+        countText.SetText($"Count: {count}");
+        scoreText.SetText($"Score: {score}");
+
+    }
+
+    private void Update()
+    {
+        if (count >= 7) 
+        { 
+            winText.SetText("You Win!");
+            speed = 0;
+            isEnd = true;
+
+        }
+        if(score < 0)
+        {
+            winText.SetText("GameOver");
+            speed = 0;
+            isEnd = true;
+        }
     }
 
     // Update is called once per frame
@@ -27,9 +61,24 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Pickups"))
+        if (collision.gameObject.CompareTag("Pickups") && !isEnd)
         {
             Destroy(collision.gameObject);
+
+            count++;
+            score += 10;
+
+            countText.SetText($"Count: {count}");
+            scoreText.SetText($"Score: {score}");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Background") && !isEnd)
+        {
+            score -= 5;
+            scoreText.SetText($"Score: {score}");
         }
     }
 }
