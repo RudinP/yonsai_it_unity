d398897b9e48095719672ee5172e37fad0f8576e
RudinP <gin04110@gmail.com>
2023-05-09 12:46:49 +0900

    230509 : RocketMouse UI
    SlideMenu UI 추가

--- UnityProjects/RocketMouse/Assets/Scripts/UIManager.cs
+++ UnityProjects/RocketMouse/Assets/Scripts/UIManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class UIManager : MonoBehaviour
 {
     public Animator startButton;
     public Animator settingsButton;
     public Animator dialog;
+    public Animator contentPnl;
+    public Animator gearImg;
 
     public void StartGame()
     {
@@ -18,5 +22,13 @@ public class UIManager : MonoBehaviour
         startButton.SetBool("isHidden", isOpen);
         settingsButton.SetBool("isHidden", isOpen);
         dialog.SetBool("isHidden", !isOpen);
+    }
+
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPnl.GetBool("isHidden");
+        contentPnl.SetBool("isHidden", !isHidden);
+
+        gearImg.SetTrigger("isClicked");
     }
 }
